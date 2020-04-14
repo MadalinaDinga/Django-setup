7d33fc6f6786c86cddc45b29608f41a35afd9670
MadalinaDinga <madadinga@gmail.com>
2020-04-14 23:13:07 +0200

    + part 4 - writing views and raising 404 error, template systems, namespacing URL names

--- djangosetup/djangosetup/settings.py
+++ djangosetup/djangosetup/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -27,17 +26,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig', # include the polls app
-    'django.contrib.admin', # The admin site.
+    'polls.apps.PollsConfig',  # include the polls app
+    'django.contrib.admin',  # The admin site.
     'django.contrib.auth',  # An authentication system.
-    'django.contrib.contenttypes', # A framework for content types.
-    'django.contrib.sessions', # A session framework.
-    'django.contrib.messages', # A messaging framework.
-    'django.contrib.staticfiles', # A framework for managing static files.
+    'django.contrib.contenttypes',  # A framework for content types.
+    'django.contrib.sessions',  # A session framework.
+    'django.contrib.messages',  # A messaging framework.
+    'django.contrib.staticfiles',  # A framework for managing static files.
 ]
 
 MIDDLEWARE = [
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangosetup.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -80,7 +77,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -100,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -113,7 +108,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
--- djangosetup/djangosetup/urls.py
+++ djangosetup/djangosetup/urls.py
@@ -1,19 +1,18 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from djangosetup.polls import views
-
 # To get from a URL to a view, Django uses what are known as ‘URLconfs’.
+from polls import views
 
 urlpatterns = [
-# ex: /polls/
+    # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('polls/<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('polls/<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
     path('polls/', include('polls.urls')),  # allow referencing other URLconfs
     path('admin/', admin.site.urls),
 ]
