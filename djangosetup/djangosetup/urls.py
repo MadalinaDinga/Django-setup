9ebaf81a9c99faa000b46877ffd412560c3d285f
MadalinaDinga <madadinga@gmail.com>
2020-04-14 23:37:36 +0200

    + part 4 - bug fix

--- djangosetup/djangosetup/urls.py
+++ djangosetup/djangosetup/urls.py
@@ -5,14 +5,6 @@ from django.urls import include, path
 from polls import views
 
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('polls/<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('polls/<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
     path('polls/', include('polls.urls')),  # allow referencing other URLconfs
     path('admin/', admin.site.urls),
 ]
